86c4e6db1e9ad398f423d19a28a1125f84039935
Max Kirillov <max@max630.net>
2014-08-12 07:05:12 +0300

    IMPL: +pa_proplist_sets

--- proplist.c
+++ proplist.c
@@ -20,9 +20,21 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 #include <pulse/proplist.h>
 #include <pulse/xmalloc.h>
 
-struct pa_entry
+#include <assert.h>
+#include <search.h>
+#include <string.h>
+
+struct pap_tree_entry
 {
     char* key;
+    char* value;
+};
+
+static int compare_entry(const void* e1, const void* e2)
+{
+    assert(e1);
+    assert(e2);
+    return strcmp(((const struct pap_tree_entry*)e1)->key, ((const struct pap_tree_entry*)e2)->key);
 };
 
 struct pa_proplist
@@ -38,5 +50,25 @@ pa_proplist* pa_proplist_new(void)
 
 void pa_proplist_free(pa_proplist* p)
 {
+    // TODO: clean correctly
     pa_xfree(p);
+}
+
+int pa_proplist_sets(pa_proplist *p, const char *key, const char *value)
+{
+    // TODO:
+    // 1. verify utf8
+    // 2. assert != NULL all (outptu from pa_.. also)
+    struct pap_tree_entry sample = { .key = (char*)key, .value = NULL };
+    struct pap_tree_entry* existing = tfind(&sample, &p->tree, &compare_entry);
+    if (existing != NULL) {
+        pa_xfree(existing->value);
+        existing->value = pa_xstrdup(value);
+    } else {
+        struct pap_tree_entry* new_entry = pa_xmalloc(sizeof(struct pap_tree_entry));
+        new_entry->key = pa_xstrdup(key);
+        new_entry->value = pa_xstrdup(value);
+        tsearch(new_entry, &p->tree, &compare_entry);
+    }
+    return 0;
 }
