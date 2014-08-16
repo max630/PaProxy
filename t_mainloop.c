43abee5de31821713b422e3ee41737fb0a5f342b
Max Kirillov <max@max630.net>
2014-08-16 14:35:33 +0300

    IMPL: simple defer test

--- t_mainloop.c
+++ t_mainloop.c
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <pulse/def.h>
 #include <pulse/mainloop.h>
 #include <pulse/mainloop-api.h>
+#include <pulse/xmalloc.h>
 
 #include "test.h"
 
@@ -28,10 +29,41 @@ static int test_create()
     pa_mainloop* m = pa_mainloop_new();
 
     pa_mainloop_free(m);
+    return 0;
+}
+
+static int test_api_defer(pa_mainloop_api* a)
+{
+    struct data_s {
+        pa_defer_event* event;
+        int count;
+    };
+    void cb(pa_mainloop_api*a, pa_defer_event* e, void *data_p)
+    {
+        struct data_s* data = data_p;
+        if (data->count != 0)
+            a->quit(a, data->count);
+        data->count++;
+    }
+    struct data_s* data = pa_xmalloc0(sizeof(struct data_s));
+    data->event = a->defer_new(a, &cb, data);
+}
+
+static int test_defer()
+{
+    pa_mainloop* m = pa_mainloop_new();
+    test_api_defer(pa_mainloop_get_api(m));
+    int ret = 0;
+    pa_mainloop_run(m, &ret);
+    ASSERT_X(ret == 1, "ret: %d", ret);
+    pa_mainloop_free(m);
+    return 0;
 }
 
 int main()
 {
+    SUB(test_create);
+    SUB(test_defer);
     
     return test_summary();
 }
