d846a723f72f4c1517348fb6d3876637b33cc8df
Max Kirillov <max@max630.net>
2014-08-16 14:07:51 +0300

    start mainloop test

--- test.h
+++ test.h
@@ -19,6 +19,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #pragma once
 
+#include <stdio.h>
+
 extern int fail_count __attribute__ ((weak));
 int fail_count = 0;
 
